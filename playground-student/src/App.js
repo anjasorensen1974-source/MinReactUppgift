1c982af10791f99d9557a9dd66ef4f6d004475f2
Anja <anja.sorensen1974@gmail.com>
2026-04-09 13:05:04 +0200

    sparar innan lärares uppdatering

--- playground-student/src/App.js
+++ playground-student/src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 import { App01_01, App01_02, App01_03 } from './apps/App01';
 import { App02_01, App02_02, App02_03 } from './apps/App02';
 import { App03_01, App03_02, App03_03 } from './apps/App03';
-import { App04_01, App04_02, App04_03 } from './apps/App03';
-import { App05_01, App05_02, App05_03 } from './apps/App03';
+import { App04_01, App04_02, App04_03 } from './apps/App04';
+import { App05_01, App05_02, App05_03 } from './apps/App05';
+import { App01_05 } from '../../m4l03/src/apps/App01';
 
 
 function App() {
   return (
 
-    <App01_01/>
+    <App01_05/>
 
   );
 }
